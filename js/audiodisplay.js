f4c840fd478b955ea38cb18b5ba8a84cbe784987
davidmorison <38760002+davidmorison@users.noreply.github.com>
2021-11-15 13:21:26 -0700

    check high freq

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -20,6 +20,7 @@ function drawBuffer( width, height, context, data ) {
 
 function plotBuffer( width, height, context, data ) {
     var step = Math.ceil( data.length / width );
+    step=1
     var amp = height / 2;
     var s_data =new Array(data.length);
     s_data[0]=Math.abs(data[0]);
@@ -32,8 +33,8 @@ function plotBuffer( width, height, context, data ) {
         var min = 1.0;
         var max = -1.0;
         for (j=0; j<step; j++) {
-            // var datum = Math.abs(  data[(i*step)+j]); 
-            var datum = Math.abs(s_data[(i*step)+j]); 
+            var datum = Math.abs(  data[(i*step)+j]); 
+            // var datum = Math.abs(s_data[(i*step)+j]); 
             if (datum < min)
                 min = datum;
             if (datum > max)
