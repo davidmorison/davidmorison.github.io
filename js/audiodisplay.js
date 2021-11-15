c97bd906d14eeec54868c6530ca47c3c838ba992
davidmorison <38760002+davidmorison@users.noreply.github.com>
2021-11-15 13:05:48 -0700

    longer time const

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -24,7 +24,7 @@ function plotBuffer( width, height, context, data ) {
     var s_data =new Array(data.length);
     s_data[0]=Math.abs(data[0]);
     for(var i=1; i<data.length; i++){
-        s_data[i]=0.9*s_data[i-1]+0.1*Math.abs(data[i])
+        s_data[i]=0.99*s_data[i-1]+0.01*Math.abs(data[i])
     }
     context.fillStyle = "green";
     context.clearRect(0,0,width,height);
