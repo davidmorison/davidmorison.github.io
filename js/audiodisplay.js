f7181d39f4628c53604471f337437fbeb1bc16b0
davidmorison <38760002+davidmorison@users.noreply.github.com>
2021-11-15 13:02:25 -0700

    smoothed

--- js/audiodisplay.js
+++ js/audiodisplay.js
@@ -21,13 +21,19 @@ function drawBuffer( width, height, context, data ) {
 function plotBuffer( width, height, context, data ) {
     var step = Math.ceil( data.length / width );
     var amp = height / 2;
+    var s_data =new Array(data.length);
+    s_data[0]=Math.abs(data[0]);
+    for(var i=1; i<data.length; i++){
+        s_data[i]=0.9*s_data[i-1]+0.1*Math.abs(data[i])
+    }
     context.fillStyle = "green";
     context.clearRect(0,0,width,height);
     for(var i=0; i < width; i++){
         var min = 1.0;
         var max = -1.0;
         for (j=0; j<step; j++) {
-            var datum = Math.abs(data[(i*step)+j]); 
+            // var datum = Math.abs(  data[(i*step)+j]); 
+            var datum = Math.abs(s_data[(i*step)+j]); 
             if (datum < min)
                 min = datum;
             if (datum > max)
