60cb68d29d97cff6548260ec95ddbfd1b8f1c020
davidmorison <38760002+davidmorison@users.noreply.github.com>
2021-11-15 15:58:58 -0700

    Add files via upload

---
+++ js/dogCatUtils.js
@@ -0,0 +1,25 @@
+function convolve(ts,wv){
+  var nt = ts.length;
+  var nw = wv.length;
+  var cs = new Array(nt);
+  var R  = Math.floor(nw/2);
+  for(var i=0; i < nt; i++){
+    x=0
+    for(var j=0; j < nw; j++){
+      var ii=i+j-R
+      var inc=0
+      if (ii<0) { inc = 0} else if (ii>=nt) {inc = 0 } else {inc = ts(ii)*wv(j)}
+      x+=inc;
+      }
+      cs[i]=x;
+    }
+    return cs;
+}
+
+
+// RATE
+// dt=1./RATE
+// lo_f=
+// hi_f=
+
+// for 
